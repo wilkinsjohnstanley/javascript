61b2ba1bf40022b273b2c4168b31fa6131121e74
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-12-14 17:47:08 -0600

    first commit

---
+++ objects.js
@@ -0,0 +1,27 @@
+// var assistantManager = {
+//     movement: 3,
+//     socialSkills: 30,
+//     streetSmarts:10,
+//     health: 40
+// }
+
+// console.log(`Hey, it's me. The man with the social skills of a Level ${assistantManager.socialSkills} Assistant Manager. ` )
+var user = {}; //create an object
+//creating an object with properties and their values
+var assistantManager = {
+    rangeTilesPerTurn: 3,
+    socialSkills: 30,
+    streetSmarts: 30,
+    health: 40,
+    specialAbility: "young and ambitious",
+    greeting: "Let's make some money"
+}
+
+var table = {
+    legs: 3,
+    color: "brown",
+    priceUSD: 100,
+}
+
+console.log(table);//display the object in the developer console
+console.log(table.color); // 'brown'
---
+++ random.js
@@ -0,0 +1,10 @@
+Math.random();
+
+//Save it to a variable
+var decimal = Math.random();
+
+//Log the value of decimal to the console
+console.log(decimal);
+
+//Log the value of decimal MULTIPLIED BY 10
+console.log(decimal*10)
