06d84d35bd564afd5a17b4d29e809b2d5dab831f
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-12-29 14:27:08 -0600

    classes are object blueprints

---
+++ classes.js
@@ -0,0 +1,9 @@
+class Car {
+    constructor(color, speed){
+        this.color = color;
+        this.speed = speed;
+    }
+    turboOn(){
+        console.log("brrrrrrrrrrrrrrr!beeeeerrrrrrrrr!brrrrrr!")
+    }
+}
