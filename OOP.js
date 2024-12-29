d82915f8294c7b6fe83c4855a7e7439616602289
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-12-29 14:21:23 -0600

    fixed my OOPsy

--- OOP.js
+++ OOP.js
@@ -2,10 +2,10 @@ var purchase = {
     shoes:100,
     stateTax:1.2,
     totalPrice:function(){
-        var calculation = purchase.shoes*purchase.stateTax;
+        var calculation = this.shoes*this.stateTax;
         console.log('Total price: ', calculation);
     }
 }
 //functions inside objects are methods
 
-console.log(purchase.totalPrice()); // Call the method
+purchase.totalPrice()
