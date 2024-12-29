051747e2bdff282ebbaca97693d0d5bc66d49561
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-12-29 16:30:20 -0600

    spreading positivity

---
+++ spread.js
@@ -0,0 +1,15 @@
+let top3 = [
+    "The Colosseum",
+    "Trevi Fountain",
+    "The Vatican City"
+];
+
+function showItinerary(place1, place2, place3){
+    console.log("First we'll stop at: " + place1)
+    console.log("After that, we'll see: " + place2)
+    console.log("Last but not least: " + place3)
+
+}
+
+//Unefficient!
+//showItinerary(top3[0], top3[1], top3[2])
