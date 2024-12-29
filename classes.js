552344a0cff01c15704646a25e0bb32fa7ed8382
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-12-29 14:32:08 -0600

    more class fun

--- classes.js
+++ classes.js
@@ -1,9 +1,20 @@
-class Car {
-    constructor(color, speed){
-        this.color = color;
-        this.speed = speed;
-    }
-    turboOn(){
-        console.log("brrrrrrrrrrrrrrr!beeeeerrrrrrrrr!brrrrrr!")
+class Bird {
+    useWings() {
+        console.log("Flying!")
     }
 }
+class Eagle extends Bird {
+    useWings() {
+        super.useWings()
+        console.log("Barely flapping!")
+    }
+}
+class Penguin extends Bird {
+    useWings() {
+        console.log("Diving!")
+    }
+}
+var baldEagle = new Eagle();
+var kingPenguin = new Penguin();
+baldEagle.useWings(); // "Flying! Barely flapping!"
+kingPenguin.useWings(); // "Diving!"
---
+++ classy.js
@@ -0,0 +1,11 @@
+class Car {
+    constructor(color, speed){
+        this.color = color;
+        this.speed = speed;
+    }
+    turboOn(){
+        console.log("brrrrrrrrrrrrrrr!beeeeerrrrrrrrr!brrrrrr!")
+    }
+}
+let car1 = new Car()
+car1.turboOn();
