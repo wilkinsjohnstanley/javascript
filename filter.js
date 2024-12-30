c4e51529db8a7fa55704b144cf130e1b6b1c8789
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-12-30 14:23:25 -0600

    wow omg

--- filter.js
+++ filter.js
@@ -1,10 +1,7 @@
 const nums = [0, 10, 20, 30, 40, 50];
 const filteredNums = nums.filter(num => num > 20);
 console.log(filteredNums); // [30, 40, 50]
-/*
-
-The filter method does not modify the original array
-
+/* The filter method does not modify the original array
 If you want to log the filtered array, 
 you need to assign the result of filter to a new variable or 
 log it directly
