54d54d729ab224e67a7ba4b41368e49d3146914d
John Wilkins <wilkinsjohnstanley@gmail.com>
2024-12-31 12:16:08 -0600

    muchh lernen ooh hahh

--- heightChecker.js
+++ heightChecker.js
@@ -1,10 +1,11 @@
 var heightChecker = function(heights) {
     let count = 0 
     let expected = [...heights].sort((a, b) => a - b);
+    console.log(expected)
     for (let i = 0 ; i < heights.length ; i ++ ){
         if(heights[i] !== expected[i])
         count++
     }
     return count
 };
-console.log(expected)
+heightChecker([1, 1, 4, 3, 2, 4])
